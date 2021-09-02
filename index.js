b9d465c702c62ab754f8a949a2586df159cc064c
jdbecerra <jdbecerra1202@users.noreply.github.com>
2021-09-02 10:57:00 -0500

    logic

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 
 const app = express();
 
+var entrada = [1,0,0,1,0,0,0,1];
+
+
+entrada.map(function(salida){
+    
+})
+
+
+
 
 app.listen(4000, () =>{
     console.log(`servidor en puerto ${4000}`)
