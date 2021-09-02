3ca85d9a2f5d3fe55d7b278e315fd8795dfde38d
jdbecerra <jdbecerra1202@users.noreply.github.com>
2021-09-02 10:30:00 -0500

    finish react test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import './styles/App.scss'
 
 
-function App() {
+class App extends React.Component {
+ 
+ constructor(){
+    super()
+    this.text = React.createRef()
+    this.changeColor = this.changeColor.bind(this)
+    this.changeColor2 = this.changeColor2.bind(this)
+
+ }
+
+ changeColor(){
+   this.text.current.style.color = 'red'
+ }
+
+ changeColor2(){
+  this.text.current.style.color = 'blue'
+}
+ render() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <main>
+      <div className="container">
+        <p className="text" ref={this.text}>Prueba Basica React</p>
+        <div className="buttons">
+          <button onClick={this.changeColor2} className="blue">Azul</button>
+          <button onClick={this.changeColor} className="red">Rojo</button>
+        </div>
+      </div>
+    </main>
   );
+ }
+  
 }
 
 export default App;
